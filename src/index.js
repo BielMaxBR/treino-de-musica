8b4abb772c5773fd59cf3a0d0f6de1ebe0b82d62
Gabriel Maximo <gbielmaximo@gmail.com>
2020-08-08 00:33:25 +0000

    mostrador de batida pronto

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 var bpm = 60
 var canvas = document.getElementById("canvas")
 var ms
+var beatpos = 0
 
 function loop() {
     ms = 1000/bpm*60
     console.log("tunts")
     draw()
+    beat()
     setTimeout(loop, ms)
+}
+
+function beat() {
+    beatpos +=1
+    if (beatpos == 4) {
+        beatpos = 0
+    }
 }
 
 function draw() {
     const ctx = canvas.getContext("2d")
     ctx.fillStyle = "#fff"
-    ctx.fillRect(10,10,30,30)
-    setTimeout(()=>{
-        ctx.clearRect(0,0,450,50)
-    }, ms/4)
+    ctx.clearRect(0,0,4,4)
+    ctx.fillRect(beatpos,0,1,1)
+
 }
 
 loop()
