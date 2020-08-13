e507b6b365d772a7892e1c99d6feaedc05331295
Gabriel Maximo <gbielmaximo@gmail.com>
2020-08-13 11:18:09 +0000

    update parte 1

--- src/index.js
+++ src/index.js
@@ -3,28 +3,34 @@ var toque = 1000/bpm*60
 var list = document.querySelector('div')
 
 function batida(tempo) {
+
     this.tempo = tempo
-    this.createCanvas = function() {
-        var quatro = document.createElement('canvas')
-        quatro.id = 'canvas'
-        quatro.width = this.tempo
-        quatro.height = 1
-        quatro.style = {"width": `${quatro.width*50}px`}
-        list.appendChild(quatro)
-        var ctx = quatro.getContext("2d")
-        ctx.fillStyle = "#fff"
-        
-        for (var i = 0; i < this.tempo; i++) {
-            if (i%2 == 0) {
-                ctx.fillRect(i,0,1,1)
-            }
-        }
+    this.batida = 0 
+
+    var quatro = document.createElement('canvas')
+
+    quatro.id = 'canvas'
+    quatro.width = this.tempo
+    quatro.height = 1
+    quatro.style = {"width": `${quatro.width*50}px`}
+    list.appendChild(quatro)
+
+    var ctx = quatro.getContext("2d")
+    ctx.fillStyle = "#fff"
+
+    this.draw = function(batida) {
+        ctx.fillRect(batida-1,0,1,1)
 
     }
 
-    this.createCanvas()
+    this.update = function() {
+        batida += 1
+        if (batida > tempo){
+            batida = 1
+        }
+        this.draw(batida)
+    }
+
 }
 
-var quatro = new batida(3)
-quatro.tempo = 4
-quatro.createCanvas()
+var quatro = new batida(4)
