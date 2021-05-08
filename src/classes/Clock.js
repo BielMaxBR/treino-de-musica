d91a96a43fc35b911ebe5668acc34ca33a0c0e20
BielMaxBR <gbielmaximo@gmail.com>
2021-05-08 00:02:55 -0300

    ta quase la mano, ta lindo dms

--- src/classes/Clock.js
+++ src/classes/Clock.js
@@ -2,22 +2,22 @@ import Circle from "./Circle.js"
 export default class Clock extends Phaser.GameObjects.GameObject {
   constructor(scene, x = 250, y = 250) {
     super(scene, 'center')
-    
+
     this.scene = scene
     this.x = x
     this.y = y
-    
+
     this.isBeating = false
-    
+
     this.patterns = scene.data.patterns
     this.track = scene.data.track
-    
+
     this.beatIndex = 0
     this.playIndex = 16
     this.patternIndex = 0
-    
-    this.pointers = scene.add.container(x, y)
-    
+
+    this.pointers = [] //scene.add.container(x, y)
+
     this.orbit = new Circle(scene, x, y, 100, 0x00, 0x000000)
     this.orbit.setStrokeStyle(1, 0xaaaaaa, 0x999999)
 
@@ -31,31 +31,58 @@ export default class Clock extends Phaser.GameObjects.GameObject {
     //  scale:{start:2,end:1},
     //  duration:300
     //})
+    //
   }
 
   run() {
+
     let pattern = this.patterns[this.patternIndex]
-    let note = pattern[this.beatIndex]
-    if (this.beatIndex<pattern.length) {
-      console.log(note)
-      this.beatIndex++
+    if (!pattern) {
+      this.patternIndex = 0
+      pattern = this.patterns[this.patternIndex]
     }
-    else {
+    let note = pattern[this.beatIndex]
+
+    this.pointers.forEach(pointer => {
+      if (pointer == 0) return;
+      let pos = Phaser.Math.RotateAroundDistance(
+        new Phaser.Geom.Point(pointer.x, pointer.y),
+        350,
+        250,
+        (2*Math.PI) / 32,
+        100
+      )
+      //console.log(pointer)
+      pointer.x = pos.x
+      pointer.y = pos.y
+
+    })
+
+    if (this.beatIndex == pattern.length) {
       this.patternIndex++
       this.beatIndex = 0
     }
-    /*let pos = Phaser.Math.RotateAroundDistance(
-      new Phaser.Geom.Point(this.pointer.x, this.pointer.y),
-      350,
-      250,
-      Math.PI / 32,
-      100
-    )
-    
 
-    this.pointer.x = pos.x
-    this.pointer.y = pos.y
+    if (note != 0) {
+      let firstValue = this.pointers[0]
+      if (!firstValue) firstValue = 0
+      if (firstValue != 0 && this.pointers.length == 32) {
+        this.pointers.push(firstValue)
+      } else {
+        let pointer = new Circle(this.scene, this.x, this.y + 100, 8)
+        this.pointers.push(pointer)
+      }
 
+    } else {
+      this.pointers.push(0)
+    }
+    if (this.pointers.length == 32) {
+      this.pointers.shift()
+    }
+    this.beatIndex++
+
+
+    /*
     if (this.pointer.x - this.center.x > -7 && this.pointer.x - this.center.x < 7 && this.pointer.y - this.center.y < 0) {
       if (!this.isBeating) {
         this.isBeating = true
